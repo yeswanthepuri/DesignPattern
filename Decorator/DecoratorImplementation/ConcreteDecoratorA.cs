1c01e12417ed09d0a12e994cac38fc65b336d4be
yeswanthepuri <epuri.yeshu@gmail.com>
2024-08-27 23:24:38 +0530

    Decorator patter

---
+++ Decorator/DecoratorImplementation/ConcreteDecoratorA.cs
@@ -0,0 +1,27 @@
+﻿
+
+using Decorator.ComponentDetails;
+
+namespace Decorator.DecoratorImplementation
+{
+    public class ConcreteDecoratorA : DecoratorClass
+    {
+        public ConcreteDecoratorA(Component component) : base(component)
+        {
+        }
+        public override string Operation()
+        {
+            return $"ConcreteDecoratorA({base.Operation()})";
+        }
+    }
+    public class ConcreteDecoratorB : DecoratorClass
+    {
+        public ConcreteDecoratorB(Component component) : base(component)
+        {
+        }
+        public override string Operation()
+        {
+            return $"ConcreteDecoratorB({base.Operation()})";
+        }
+    }
+}
---
+++ Decorator/DecoratorImplementation/DecoratorClass.cs
@@ -0,0 +1,37 @@
+﻿using Decorator.ComponentDetails;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decorator.DecoratorImplementation
+{
+    /// <summary>
+    /// Decorator Implementation of Component
+    /// </summary>
+    public class DecoratorClass : Component
+    {
+        protected Component _component;
+        public DecoratorClass(Component component)
+        {
+            _component = component;
+        }
+        public void SetComponent(Component component)
+        {
+            this._component = component;
+        }
+        public override string Operation()
+        {
+            if (this._component != null)
+            {
+
+                return this._component.Operation();
+            }
+            else
+            {
+                return string.Empty;
+            }
+        }
+    }
+}
